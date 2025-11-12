631876e6c7b14b570ce5bd4b28f27abb3f874998
biplobrana007 <mubiplobrana@gmail.com>
2025-11-12 21:49:51 +0600

    mongodb connected

--- index.js
+++ index.js
@@ -1,14 +1,45 @@
 const express = require("express");
 const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require("mongodb");
+
 const app = express();
 const port = 3000;
+
 app.use(cors());
 app.use(express.json());
+
+const uri =
+  "mongodb+srv://aimodel-db:qsHVvnKJCgPQJWyc@aimodelcluster.xnrsyug.mongodb.net/?appName=AiModelCluster";
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    await client.connect();
+
+
+
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // await client.close();
+  }
+}
+run().catch(console.dir);
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
 app.listen(port, () => {
-  console.log(`app listening on port ${port}`);
+  console.log(`Server is listening on port ${port}`);
 });
