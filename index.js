b75a3370df8f4ac9958bb79e528b2ed7807d3deb
biplobrana007 <mubiplobrana@gmail.com>
2025-11-13 12:52:15 +0600

    update model api created

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 
 const app = express();
 const port = 3000;
@@ -26,18 +26,41 @@ async function run() {
 
     const db = client.db("aimodel-db");
     const modelCollections = db.collection("models");
-
+    //get models
     app.get("/models", async (req, res) => {
       const result = await modelCollections.find().toArray();
       res.send(result);
     });
 
+    // add new model
     app.post("/models", async (req, res) => {
       const data = req.body;
       const result = await modelCollections.insertOne(data);
       res.send({
         success: true,
       });
+    });
+
+    //show model details
+    app.get("/models/:id", async (req, res) => {
+      const { id } = req.params;
+      const result = await modelCollections.findOne({ _id: new ObjectId(id) });
+      res.send({
+        success: true,
+        result,
+      });
+    });
+
+    //update model
+    app.put("/models/:id", async (req, res) => {
+      const { id } = req.params;
+      const data = req.body;
+      const filter = { _id: new ObjectId(id) };
+      const update = { $set: data };
+
+      const result = await modelCollections.updateOne(filter, update);
+
+      res.send({ success: true, result });
     });
 
     await client.db("admin").command({ ping: 1 });
