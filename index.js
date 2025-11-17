0cf5b4563751a6d87f799ba19e2f57c1c99d925a
biplobrana007 <mubiplobrana@gmail.com>
2025-11-17 23:27:06 +0600

    secured mogodb password and username by dotenv

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 const express = require("express");
 const cors = require("cors");
-const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+require("dotenv").config();
 const app = express();
 const port = 3000;
 
 app.use(cors());
 app.use(express.json());
 
-const uri =
-  "mongodb+srv://aimodel-db:qsHVvnKJCgPQJWyc@aimodelcluster.xnrsyug.mongodb.net/?appName=AiModelCluster";
+const uri = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@aimodelcluster.xnrsyug.mongodb.net/?appName=AiModelCluster`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -121,7 +121,6 @@ async function run() {
 
       res.send(result);
     });
-
 
     await client.db("admin").command({ ping: 1 });
     console.log(
